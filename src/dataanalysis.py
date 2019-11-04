0e032273f01fdb1b472ff784cdf43bbd2d62b061
alex <21alex295@gmail.com>
2019-11-04 19:43:36 +0100

    Saves separated data into csv files

--- src/dataanalysis.py
+++ src/dataanalysis.py
@@ -1,37 +1,39 @@
 from dataframemanager import DataframeManager
 import matplotlib.pyplot as plt
 import math
+import pandas as pd
 
 path = "/home/alex/Documents/Clase/TFG/Dataset/iskra/"
 
-aa = DataframeManager.get_all_dataframes(path)
+unseparated_df_list = DataframeManager.get_all_dataframes(path)
+
+df_index = 0
+for unseparated_df in unseparated_df_list:
+    towing_data_only = unseparated_df.where\
+        (unseparated_df["Escalas(m)Estribor"] >
+         unseparated_df["Escalas(m)Estribor"].mean())
+
+    i = 0
+    nonnan_index = []
+    while i < len(towing_data_only):
+        if not math.isnan(towing_data_only["Escalas(m)Estribor"][i]):
+            nonnan_index.append(i)
+        i += 1
+
+    end_index = []
+    start_index = []
+    start_index.append(nonnan_index[0])
+    for i in range(1, len(nonnan_index) - 1):
+        if nonnan_index[i] + 1 != nonnan_index[i + 1]:
+            end_index.append(nonnan_index[i])
+        if nonnan_index[i] - 1 != nonnan_index[i - 1]:
+            start_index.append(nonnan_index[i])
+    end_index.append(nonnan_index[len(nonnan_index) - 1])
 
 
-a = aa[1]
-
-itemdrop =a.where(a["Escalas(m)Estribor"] > a["Escalas(m)Estribor"].mean())
-
-i = 0
-nonnan_index = []
-while i < len(itemdrop):
-    if not math.isnan(itemdrop["Escalas(m)Estribor"][i]):
-        nonnan_index.append(i)
-    i += 1
-
-end_index = []
-start_index = []
-start_index.append(nonnan_index[0])
-for i in range(1, len(nonnan_index) - 1):
-    if nonnan_index[i] + 1 != nonnan_index[i + 1]:
-        end_index.append(nonnan_index[i])
-    if nonnan_index[i] - 1 != nonnan_index[i - 1]:
-        start_index.append(nonnan_index[i])
-end_index.append(nonnan_index(len(nonnan_index) - 1))
-for i in range(len(end_index)):
-    df = a[start_index[i]:end_index[i]]
-
-"""
-fig, axs = plt.subplots(2)
-fig.suptitle('Vertically stacked subplots')
-axs[0].plot(itemdrop["Escalas(m)Estribor"])
-axs[1].plot(a["Escalas(m)Estribor"])"""
+    for i in range(len(end_index)):
+        df = unseparated_df[start_index[i]:end_index[i]]
+        df.to_csv("/home/alex/Documents/Clase/TFG/Dataset/"
+                  + str(df_index) + "-" + str(i) + ".csv" )
+    df_index += 1
+    print(df_index)

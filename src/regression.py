4e66b925f7ff2bc697a0015f9b9c715e3ac136e2
alex <21alex295@gmail.com>
2019-12-19 16:34:13 +0100

    Fixes inits in modules

--- src/regression.py
+++ src/regression.py
@@ -1,45 +1,46 @@
 from sklearn.model_selection import train_test_split
-from models import randomforest, supportvectorregression as svr, \
+from src.models import randomforest, supportvectorregression as svr, \
     linearregression
-from utils import windowroll, dfopener, scaler
+from src.utils import windowroll, dfopener, scaler
 import numpy as np
 
 
-df_path = "/home/alex/Documents/Clase/TFG/Dataset/csv_files_apertura/"
-dfs = dfopener.df_opener(df_path, min_sensor_reads=50)
+def main():
+    df_path = "Dataset/csv_files_apertura/"
+    dfs = dfopener.df_opener(df_path, min_sensor_reads=50)
 
-scaled_dfs, df_scaler = scaler.fit_scale(dfs)
-train, test = train_test_split(scaled_dfs, test_size=0.4, random_state=42)
+    scaled_dfs, df_scaler = scaler.fit_scale(dfs)
+    train, test = train_test_split(scaled_dfs, test_size=0.4, random_state=42)
 
 
-rfr_rmse = []
-lr_rmse = []
-svr_rmse = []
-kernels = ["rbf", "linear", "poly"]
+    rfr_rmse = []
+    lr_rmse = []
+    svr_rmse = []
+    kernels = ["rbf", "linear", "poly"]
 
-#hiperparametrizacion
-print("--------ADESTRAMENTO DE ALGORITMOS---------\n")
-for gap in range(0, 50, 5):
-    print("\n\n\n---Distancia de predicion => " + str(gap))
-    X_train, y_train = windowroll.map_window(train, gap=gap)
-    X_test, y_test = windowroll.map_window(test, gap=gap)
+    #hiperparametrizacion
+    print("--------ADESTRAMENTO DE ALGORITMOS---------\n")
+    for gap in range(0, 50, 5):
+        print("\n\n\n---Distancia de predicion => " + str(gap))
+        X_train, y_train = windowroll.map_window(train, gap=gap)
+        X_test, y_test = windowroll.map_window(test, gap=gap)
 
-    print("\n\nErros para RandomForest ->\n")
-    for i in  range(100, 250, 50):
-        rfr_rmse.append(
-            randomforest.fit_predict_rfr(
-                X_train,y_train, X_test, y_test, i))
+        print("\n\nErros para RandomForest ->\n")
+        for i in  range(100, 250, 50):
+            rfr_rmse.append(
+                randomforest.fit_predict_rfr(
+                    X_train,y_train, X_test, y_test, i))
 
-    print("\n\nErros para linear regression ->\n")
-    lr_rmse.append(
-        linearregression.fit_predict_lr(X_train, y_train, X_test, y_test))
+        print("\n\nErros para linear regression ->\n")
+        lr_rmse.append(
+            linearregression.fit_predict_lr(X_train, y_train, X_test, y_test))
 
-    print("\n\nErros para SVR ---->\n")
-    for kernel in kernels:
-        for degree in range(2, 6):
-            for coefficient in range(1,5):
-                for epsilon in np.arange(0.1, 1.5, 0.2):
-                    svr_rmse.append(
-                        svr.fit_predict_lr(X_train,y_train, X_test, y_test,
-                                           kernel, degree, coefficient, epsilon)
-                    )
+        print("\n\nErros para SVR ---->\n")
+        for kernel in kernels:
+            for degree in range(2, 6):
+                for coefficient in range(1,5):
+                    for epsilon in np.arange(0.1, 1.5, 0.2):
+                        svr_rmse.append(
+                            svr.fit_predict_lr(X_train,y_train, X_test, y_test,
+                                               kernel, degree, coefficient, epsilon)
+                        )

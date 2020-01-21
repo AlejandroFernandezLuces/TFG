e1c1c4e528052596b60bb0406a8c41cabd89cbf9
alex <21alex295@gmail.com>
2020-01-21 18:20:34 +0100

    Bugfixing from previous version

--- src/models/supportvectorregression.py
+++ src/models/supportvectorregression.py
@@ -13,7 +13,7 @@ def fit_predict_svr(X_train, y_train, X_test, y_test):
                  'epsilon': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1,
                              0.5, 1, 5, 10],
                  'gamma': [0.0001, 0.001, 0.005, 0.1, 1, 3, 5]}
-    gs = GridSearchCV(SVR(), param_grid=param_grid, verbose=2)
+    gs = GridSearchCV(SVR(), param_grid=param_grid, verbose=2, n_jobs=-1)
     gs.fit(X_train, y_train)
     pred = gs.best_estimator_.predict(X_test)  # make prediction on test set
     error = sqrt(mean_squared_error(y_test, pred))  # calculate rmse

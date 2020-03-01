23db76e2394f952483b37b0b11ae6a9464d89c6e
21alex295ç <21alex295@gmail.com>
2020-03-01 14:52:24 +0100

    Updates API reference, Generates server stub

---
+++ web/server_stub/swagger_server/models/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.body import Body
+from swagger_server.models.body1 import Body1
+from swagger_server.models.inline_response200 import InlineResponse200
